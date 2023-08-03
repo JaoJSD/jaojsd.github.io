58f593254d5bb95f3774a452161a66119d6307f1
jair socarras <jjsd204@gmail.com>
2023-08-03 11:26:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,17 +11,6 @@ myImage.onclick = () => {
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 
-myButton.onclick = () => {
-    setUserName();
-};
-
-
-if (!localStorage.getItem("name")) {
-    setUserName();
-} else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
-}
 
 function setUserName() {
     const myName = prompt("Please enter your name.");
@@ -32,6 +21,17 @@ function setUserName() {
         myHeading.textContent = `Mozilla is cool, ${myName}`;
     }
 }
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
 
 
 
